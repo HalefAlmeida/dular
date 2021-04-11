ebf56badac793cbcbfb00861edcc7a3304bfce5f
halefalmeida <halef.dorigan@outlook.com>
2021-04-11 12:14:51 -0300

    added generic no itens  component to be displayed if the observable have no itens in cart added cart itens service

--- src/app/features/shopping-cart/shopping-cart.component.ts
+++ src/app/features/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CartItem, CartItensService } from './cart-itens.service';
 import { Cart, ShoppingCartService } from './shopping-cart.service';
 
 @Component({
@@ -10,13 +11,16 @@ import { Cart, ShoppingCartService } from './shopping-cart.service';
 export class ShoppingCartComponent implements OnInit {
 
   cart$: Observable<Cart[]>
+  cartItens$: Observable<CartItem[]>
 
   constructor(
-    private cartService: ShoppingCartService
+    private cartService: ShoppingCartService,
+    private cartItensService: CartItensService
   ) { }
 
   ngOnInit(): void {
     this.cart$ = this.cartService.list()
+    this.cartItens$ = this.cartItensService.list()
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { NoItensComponent } from './components/no-itens/no-itens.component';
 
 
 
 @NgModule({
   declarations: [
-    SpinnerComponent
+    SpinnerComponent,
+    NoItensComponent
   ],
   imports: [
     CommonModule,
     MatProgressSpinnerModule
   ],
   exports: [
-    SpinnerComponent
+    SpinnerComponent,
+    NoItensComponent
   ]
 })
 export class SharedModule { }
